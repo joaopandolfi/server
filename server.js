be217049a3c30bed07a18632250b77d09840c42c
João Carlos Pandolfi Santana <joaopandolfi@gmail.com>
2019-10-08 23:18:09 -0300

    removing debug

--- server.js
+++ server.js
@@ -155,11 +155,9 @@ export default function(opt) {
         }
         var paths = req.url.split("/")
         var clientId = ""
-        console.log(req.url)
         if(paths.length >= 3 && paths[1] == "t"){
             clientId = paths[2]
             req.url = req.url.replace(`/t/${clientId}`,"")
-            console.log(req.url);
         }else {
             clientId = GetClientIdFromHostname(hostname);
         }
